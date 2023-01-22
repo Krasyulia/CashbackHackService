1e935e770e926a32bf5af48ea8bc72252f821729
Krasyulia <krasjulia@gmail.com>
2023-01-22 21:58:03 +0400

    junit4 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,25 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
-    void remain() {
+    public void shouldRecommendSpend1() {
+        Assert.assertEquals(1, service.remain(999));
     }
+
+    @Test
+    public void shouldRecommendSpend0() {
+        Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @Test
+    public void shouldRecommendSpend1000() {
+        Assert.assertEquals(1000, service.remain(0));
+    }
+
 }
