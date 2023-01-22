c37583a798730ef27fe763bde20fa810cfdb0ae6
Krasyulia <krasjulia@gmail.com>
2023-01-22 22:06:16 +0400

    testng tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,25 +1,23 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldRecommendSpend1() {
-        Assertions.assertEquals(1, service.remain(999));
+        Assert.assertEquals(service.remain(999), 1);
     }
 
     @Test
     public void shouldRecommendSpend0() {
-        Assertions.assertEquals(0, service.remain(1000));
+        Assert.assertEquals(service.remain(1000), 0);
     }
 
     @Test
     public void shouldRecommendSpend1000() {
-        Assertions.assertEquals(1000, service.remain(0));
+        Assert.assertEquals(service.remain(0), 1000);
     }
 }
